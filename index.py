9bac32c1182e07efdbd25bdcd860d345b0521641
Rory Straubel <rmstraub@mtu.edu>
2015-03-02 19:35:19 +0000

    Added some basic auth stuff, eventually going to move to another py file

--- index.py
+++ index.py
@@ -1,11 +1,33 @@
 from cgi import escape
 from urllib import unquote
+from tweepy.streaming import StreamListener
+from tweepy import Stream
 import json
+import tweepy
 from urllib2 import Request, urlopen, URLError
 
 # The Publisher passes the Request object to the function
 def index():
+   consKey = "aOfQrwx793BVuTRMoO3UzJMUW"
+   consSecret = "AEeq70OpKzyJdP4W8OOHN5dx2IwD6NVkbI9VEosMbSuzbkoocE"
+   accessToken = "3065054746-ksthvpPy5MTMO88F7pwS8J5oGv3mVkC7Dy1RRd3"
+   accessTokenSecret = "8KqxINVNcyCzqP9HV7TkERgcC8QFadL1Xo9Fz0R8ZKpKl"
+   auth = tweepy.OAuthHandler(consKey,consSecret)
+   auth.set_access_token(accessToken, accessTokenSecret)
+   api = tweepy.API(auth)
+   info = "test"
+   request = Request('https://api.twitter.com/1.1/trends/place.json?id=1')
+
+   try:
+       	response = urlopen(request)
+        data=json.load(response)
+        info  = data.trends[0].name
+   except URLError, e:
+        print 'Error', e
+   
+
    s = """\
+
     <html>
             <head>
                     <title>Mood Radar</title>
@@ -48,15 +70,7 @@ def index():
     </html>
     """
 	
-   request = Request('https://api.twitter.com/1.1/trends/1')
-   
-   try:
-	response = urlopen(request)
-	data=json.load(response)
-	print data
-   except URLError, e:
-	print 'Error', e
 
-   t = "<h1>TEST TWO</h1>"
+   t = "<h1>" + info +"</h1>"
    
    return s % t
